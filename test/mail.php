dbb9d704eb94ddbe6bb126ce5e352abfd5cf99ca
Nishan Ahmed <nishanahmad@gmail.com>
2018-09-22 13:02:11 +0530

    Test

--- test/mail.php
+++ test/mail.php
@@ -1,19 +1,20 @@
 <?php
-    $to = 'demo@spondonit.com';
-    $firstname = $_POST["fname"];
+	var_dump($_POST);
+    $to = 'info@meracinternational.com';
+    $company = $_POST["company"];
+	$contact = $_POST["contact"];
+	$phone = $_POST["phone"];
+	$whatsapp = $_POST["whatsapp"];
     $email= $_POST["email"];
     $text= $_POST["message"];
-    $phone= $_POST["phone"];
     
-
-
     $headers = 'MIME-Version: 1.0' . "\r\n";
     $headers .= "From: " . $email . "\r\n"; // Sender's E-mail
     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
     $message ='<table style="width:100%">
         <tr>
-            <td>'.$firstname.'  '.$laststname.'</td>
+            <td>'.$company.' - '.$contact.'</td>
         </tr>
         <tr><td>Email: '.$email.'</td></tr>
         <tr><td>phone: '.$phone.'</td></tr>
